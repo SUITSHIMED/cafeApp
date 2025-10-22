3de0e048c76c946b05c54ba7ace9ceb1dec9ad76
youssef lab20-u <yousseflab20@gmail.com>
2025-10-22 04:20:29 +0100

    ajouter card prodacte

---
+++ page/CardProdacte.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import MenuPage from "./MenuPage";
+
+export default function CardProdacte() {
+  return (
+    <View>
+      <MenuPage Name="Youssef" />
+    </View>
+  );
+}
--- page/MenuPage.js
+++ page/MenuPage.js
@@ -1,11 +1,80 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, ImageBackground } from 'react-native';
 
 export default function MenuPage({ navigation }) {
   return (
-    <View>
-      <Text>salam</Text>
+    <ImageBackground
+      source={require('../assets/img1.jpeg')}  
+      style={styles.background}
+    >
+      <Text style={styles.title}>Motre Menu</Text>
+
+      <View style={styles.prodacte}>
+        <View style={styles.card}>
+          <Image source={require('../assets/img1.jpeg')} style={styles.image}/>
+          <Text>Espresso</Text>
+          <Text>$20</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Image source={require('../assets/img2.jpeg')} style={styles.image}/>
+          <Text>Cappuccino</Text>
+          <Text>$15</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Image source={require('../assets/img3.jpeg')} style={styles.image}/>
+          <Text>Late</Text>
+          <Text>$30</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Image source={require('../assets/img1.jpeg')} style={styles.image}/>
+          <Text>Mocha</Text>
+          <Text>$20</Text>
+        </View>
+      </View>
+
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-    </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',  
+    paddingTop: 30,
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: "center",
+    backgroundColor: "rgba(250, 135, 3, 0.7)", 
+    fontSize: 35,
+    margin: 20,
+    borderRadius: 35,
+    padding: 25,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  prodacte: {
+    flexDirection: 'row',     
+    flexWrap: 'wrap',
+    justifyContent: 'space-around', 
+    paddingHorizontal: 10,
+  },
+  card: {
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    width: 150, 
+    padding: 10,
+    marginVertical: 10, 
+    borderRadius: 15,
+    alignItems: 'center',
+  },
+  image:{
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+});
